7359462b2ccea69763eeee804f163253b367fd4c
uyriq <uyriq@local>
2022-08-28 23:57:03 +0300

    solved task 3/5

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
+/* eslint-disable indent */
 import React, { StrictMode } from 'react'
-import { createStore } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import { createRoot } from 'react-dom/client'
+import thunk from 'redux-thunk'
 import App from './components/app/app'
 import './index.css'
 
 // Корневой редьюсер, который обрабатывает экшены
 import { rootReducer } from './services/reducers'
 
-// Инициализируем хранилище с помощью корневого редьюсера
-const store = createStore(rootReducer)
+const composeEnhancers =
+    typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+        ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+        : compose
 
+const enhancer = composeEnhancers(applyMiddleware(thunk))
+const store = createStore(rootReducer, enhancer)
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
 
 root.render(
     // Оборачиваем приложение компонентом Provider из пакета react-redux
+    // eslint-disable-next-line react/jsx-filename-extension
     <StrictMode>
         <Provider store={store}>
             <App />
